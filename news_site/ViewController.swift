cd1ab3c33b9b935fe7236466c50c3a42dddb666e
GO.DRE <go.dre@GODREs-MacBook-Pro.local>
2018-12-06 09:59:22 +0300

    Initial Commit

---
+++ news_site/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  news_site
+//
+//  Created by GO.DRE on 6.12.2018.
+//  Copyright © 2018 com.ceren.ozgur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
